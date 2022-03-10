3ba9910937c491b7973c9f5a8b191e544ed0f3a0
ahugh88 <71119053+ahugh88@users.noreply.github.com>
2022-03-10 10:22:19 -0800

    the oG files, very basic still being designed

---
+++ script.js
@@ -0,0 +1,33 @@
+const multiStepForm = document.querySelector("[data-multi-step]")
+const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
+let currentStep = formSteps.findIndex(step => {
+    return step.classList.contains("active")
+})
+
+if (currentStep < 0) {
+    currentStep = 0
+    formSteps[currentStep].classList.add("active")
+}
+multiStepForm.addEventListener("click", e => {
+    let incrementor 
+    if (e.target.matches("[data-next]")) {
+        incrementor = 1
+    }   else if (e.target.matches("[data-previous]")) {
+        incremenetor = -1
+    }   
+    
+    if (incrementor == null) return
+    const inputs = [...formSteps[currentStep].querySelectorAll("input")]
+    const allValid = inputs.every(input => input.checkValidity())
+    console.log(inputs)
+    if (allValid)   {
+        currentStep += incrementor
+    showCurrentStep()
+    }
+})
+
+function showCurrentStep() {
+    formSteps.forEach((step, index) => {
+        step.classList.toggle("active", index === currentStep)
+    })
+}
